70d261c5e8793119b8a77e1c07ba0db72637dc64
edruosq <1767911220@qq.com>
2022-11-07 15:20:25 +0800

    绑定div点击事件

--- js/public.js
+++ js/public.js
@@ -1,3 +1,11 @@
 function ajax(){
     //ajax封装
 }
+window.onload=function(){
+    var div=document.getElementById("div");
+    div.onclick=function(){
+        var p=document.createElement("p");
+        p.innerText="好好学习，天天向上";
+        div.appendChild(p);
+    }
+}
